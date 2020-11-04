1826ab938924f2b33bd22b508ac1012b8cf80ae9
Ketan S. Khare <11049950+ketankhare@users.noreply.github.com>
2020-11-04 07:57:55 -0500

    Add files via upload

--- mol2tolammps/mol2tolammps.sh
+++ mol2tolammps/mol2tolammps.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 if [ "${BASH_SOURCE[0]}" -ef "$0" ]; then
-  echo "Usage: source mol2lammps.sh Name.mol2 [charge] [integer/skip]";
+  echo "Usage: ./mol2lammps.sh Name.mol2 [charge] [integer/skip]";
 else
   echo "Execute this script! Don't source!"; return
 fi
 
-DEBUG=0 #1 will retain intermediate files
+DEBUG=1 #1 will retain intermediate files
 
 #Usage Eg 1: source mol2lammps.sh Name.mol2 
 #Usage Eg 2: source mol2lammps.sh Name.mol2 charge -1
@@ -17,9 +17,8 @@ Main ()
   CheckArguments $1 $2 $3
   CheckExecutables 
   Mol2toLAMMPS $1 $2 $3
+  GenerateInputScript $1
 }
-
-#Section 1: Check if the arguments are sane and all necessary executables and scripts are available
 
 CheckArguments() {
   if [ -z "$1" ]; then
@@ -47,8 +46,6 @@ CheckExecutables() {
   if [ ! -f amber2lammps.py3 ]; then echo "Error: amber2lammps.py3 does not exist in current directory" & exit 1; fi
 }
 
-# # #Section 2: Process
-
 Mol2toLAMMPS () {
   BaseName=${1%.mol2}
 
@@ -68,6 +65,45 @@ Mol2toLAMMPS () {
   if [ "${DEBUG}" -eq 0 ]; then
     rm ${BaseName}.top ${BaseName}.crd leaprc.${BaseName} sqm.in sqm.out sqm.pdb ${BaseName}bcc.mol2 ANTECHAMBER.FRCMOD
   fi
-}  
+}
+
+GenerateInputScript() {
+  BaseName=${1%.mol2}
+  cat > in.${BaseName} <<EndInput
+units  real
+neigh_modify  delay 5 every 1
+atom_style  full
+pair_style  lj/cut/coul/long 9.0
+pair_modify  mix arithmetic tail yes
+bond_style  harmonic
+angle_style  harmonic
+dihedral_style  charmm
+improper_style  cvff
+kspace_style  pppm 1e-04
+kspace_modify  diff ad
+boundary  p p p
+special_bonds  amber
+  
+read_data Data.${BaseName}
+  
+minimize 0 0 10000 10000
+  
+replicate ${NX} ${NY} ${NZ}
+  
+variable MassG equal mass(all)/6.02214e23
+variable Volcm3 equal vol*1.0e-24
+variable Density equal v_MassG/v_Volcm3
+  
+thermo_style  custom step temp press vol etotal epair emol evdwl ecoul elong ebond eangle enthalpy pe ke v_Density lx ly lz cpu
+thermo  1000
+
+fix  MOMENTUM all momentum 100000 linear 1 1 1 angular
+fix  SHAKE all shake 0.00001 25 0 b 4 7 8 10 11 15 17 a 3 6 10 13 14 15 16 17 18 19 25 27 28 32 m 1.008
+fix  NPT all npt temp ${Temperature} ${Temperature} 100 iso 1.0 1.0 1000
+fix  RECENTER all recenter INIT INIT INIT units box
+  
+run 10000
+EndInput
+}
 
 Main $1 $2 $3
