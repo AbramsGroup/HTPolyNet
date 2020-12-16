fc941ed3825f34c0d0d46651cdfbf5ca8582ddf7
Ketan Khare <11049950+ketankhare@users.noreply.github.com>
2020-12-16 06:05:05 -0500

    Upgrade based on suggestions

---
+++ lammps-module/lammps_module_main.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Main file for the LAMMPS module of HTPolyNet utility"""
+
+import sys
+import os
+import argparse
+import ConfigParser
+
+
+'''Create template'''
+
+if __name__ == '__main__':
+
+    #Parse arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--config')
+    args=parser.parse_args()
+    print(args)
