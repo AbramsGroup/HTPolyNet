26a142a46a0c643255e8e82375689261ad68b2ee
Cameron Abrams <cfa22@drexel.edu>
2022-06-07 16:53:23 -0400

    plot

--- HTPolyNet/plot.py
+++ HTPolyNet/plot.py
@@ -46,6 +46,7 @@ def network_graph(G,filename,**kwargs):
     logging.disable(logging.NOTSET)
     
 def cure_graph(logfile,filename,**kwargs):
+    xmax=kwargs.get('xmax',-1)
     logging.disable(logging.DEBUG)
     with open(logfile,'r') as f:
         lines=f.read().split('\n')
@@ -56,8 +57,14 @@ def cure_graph(logfile,filename,**kwargs):
     data['conv']=[]
     data['iter']=[]
     for l in lines:
+        # CURE iteration 1/150 begins in
         tok=l.split()
         if len(tok)>2:
+            if len(tok)>6:
+                if tok[2]=='CURE' and tok[5]=='begins' and tok[4][0:2]=='1/':
+                    data['time'].append(datetime.strptime(' '.join(tok[0:2]),'%Y-%m-%d %H:%M:%S,%f'))
+                    data['conv'].append(0.0)
+                    data['iter'].append(0)
             if tok[2]=='Iter' and tok[4]=='current':
                 data['time'].append(datetime.strptime(' '.join(tok[0:2]),'%Y-%m-%d %H:%M:%S,%f'))
                 data['conv'].append(float(tok[6]))
@@ -66,6 +73,8 @@ def cure_graph(logfile,filename,**kwargs):
     df['elapsed']=(df['time']-df.loc[0]['time']).astype(int)/1.e9/3600.0
     fig,ax=plt.subplots(1,2,sharex=True,figsize=(10,6))
     ax[0].set_ylim([0,1])
+    if xmax>-1:
+        ax[0].set_xlim([0,xmax])
     ax[0].plot(df['elapsed'],df['conv'])
     ax[0].set_xlabel('runtime (h)')
     ax[0].set_ylabel('conversion')
