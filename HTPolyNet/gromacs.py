525f5bb7f2623f39a50e743c94f6bb07724e44ff
Cameron Abrams <cfa22@drexel.edu>
2022-08-25 15:08:29 -0400

    testing

--- HTPolyNet/gromacs.py
+++ HTPolyNet/gromacs.py
@@ -168,7 +168,7 @@ def gmx_energy_trace(edr,names=[],report_averages=False,**kwargs):
         colnames.insert(0,'time (ps)')
         # print(f'in gmx {edr}.edr {names}:')
         data=pd.read_csv(f'{edr}-out.xvg',sep='\s+',header=None,names=colnames)
-        # print(f'{data.head().to_string()}')
+        print(f'{data.head().to_string()}')
         data.iloc[:,0]+=xshift
         ndata=data.shape[0]
         if report_averages:
