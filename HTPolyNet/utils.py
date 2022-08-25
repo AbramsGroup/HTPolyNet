4678a1168b3b510d766346744ed82d375087a154
Cameron Abrams <cfa22@drexel.edu>
2022-08-25 15:41:38 -0400

    test

--- HTPolyNet/utils.py
+++ HTPolyNet/utils.py
@@ -125,8 +125,13 @@ def density_evolution(proj_dir):
                         if eq_present:
                             for ens,qtys in _md_ensembles.items():
                                 edr_pfx=os.path.join(iter_subd,pfx.format(ens=ens))
+                                gro=edr_pfx+r'.gro'
                                 # print(edr_pfx,os.path.exists(edr_pfx+r'.edr'),ens,qtys)
                                 if os.path.exists(edr_pfx+r'.edr'):
+                                    density=0.0
+                                    if ens!='npt':
+                                        if os.path.exists(gro):
+                                            density=density_from_gro(gro,os.path.join(proj_dir,'molecules/parameterized'))
                                     df,xshift=_concat_from_edr(df,edr_pfx,qtys,add=[('nbonds',nbonds)])
                                     transition_times.append(xshift)
                                     interval_labels.append([edr_pfx])
