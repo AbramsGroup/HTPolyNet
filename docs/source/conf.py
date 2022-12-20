de5d34636ba115073a6deabc2771e542aaa1244b
Cameron Abrams <cfa22@drexel.edu>
2022-12-20 14:06:31 -0500

    sys.path.insert(0, os.path.abspath('../../'))

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../'))
+sys.path.insert(0, os.path.abspath('../../'))
 # dnw:
 #from HTPolyNet.__init__.py import HTPOLYNET_VERSION
 HTPOLYNET_VERSION='1.0.5'
