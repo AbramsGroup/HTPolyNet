86070941ad50de0dccb03bb60f01ea8b1f8b4ae6
Cameron Abrams <cfa22@drexel.edu>
2023-01-05 10:55:31 -0500

    deform

--- HTPolyNet/postsim.py
+++ HTPolyNet/postsim.py
@@ -76,17 +76,33 @@ class PostSimMD:
         local_grx=os.path.basename(p['input_grx'])
         TC=TopoCoord(topfilename=local_top,grofilename=local_gro,grxfilename=local_grx)
         logger.info(f'{TC.Coordinates.A.shape[0]} atoms {TC.total_mass(units="gromacs"):.2f} amu')
+        box=TC.Coordinates.box
         pfs.checkout('mdp/npt.mdp')
         os.rename('npt.mdp',f'{mdp_pfx}.mdp')
-        self.build_mdp(f'{mdp_pfx}.mdp')
+        self.build_mdp(f'{mdp_pfx}.mdp',box=box)
         msg=TC.grompp_and_mdrun(out=p['output_deffnm'],mdp=mdp_pfx,quiet=False,mylogger=logger.info,**gromacs_dict)
         df=gmx_energy_trace(p['output_deffnm'],p['traces'])
+        use_scatter=p['scatter']
+        temp_x=None
+        temp_y=None
+        scat_file=use_scatter[2]
+        for sznm in ['Box-X','Box-Y','Box-Z']:
+            if sznm in p['traces']:
+                L0=box[0][0] if sznm=='Box-X' else box[1][1] if sznm=='Box-Y' else box[2][2]
+                temp_x=f'{sznm}-strain'
+                df[temp_x]=df[sznm]/L0-1.0
+        for sznm in ['Pres-XX','Pres-YY','Pres-ZZ']:
+            if sznm in p['traces']:
+                df[f'{sznm}-stress']=df[sznm]*(-1)
+                temp_y=[f'{sznm}-stress']
+        if temp_x and temp_y:
+            use_scatter=(temp_x,temp_y,scat_file)
         df.to_csv(f'{p["output_deffnm"]}.csv',header=True,index=False)
-        scatter(df,*p['scatter'])
+        scatter(df,*use_scatter)
         logger.info(f'Final coordinates in {p["output_deffnm"]}.gro')
         logger.info(f'Traces saved in {p["output_deffnm"]}.csv')
 
-    def build_mdp(self,mdpname):
+    def build_mdp(self,mdpname,**kwargs):
         """build_mdp builds the GROMACS mdp file required for an NPT equilibration
 
         :param mdpname: name of mdp file
@@ -132,7 +148,7 @@ class PostSimAnneal(PostSimMD):
         'T0_ps': 1000,
         'P':1
     }
-    def build_mdp(self,mdpname):
+    def build_mdp(self,mdpname,**kwargs):
         """build_mdp builds the GROMACS mdp file required for an annealing MD simulation
 
         :param mdpname: name of mdp file
@@ -187,7 +203,7 @@ class PostSimLadder(PostSimMD):
         'P':1
     }
 
-    def build_mdp(self,mdpname):
+    def build_mdp(self,mdpname,**kwargs):
         """build_mdp builds the GROMACS mdp file required for a temperature-ladder MD simulation
 
         :param mdpname: name of mdp file
@@ -225,10 +241,10 @@ class PostSimLadder(PostSimMD):
         mdp_modify(mdpname,mod_dict)
 
 class PostSimDeform(PostSimMD):
-    """ a class to handle a temperature-ladder MD simulation
+    """ a class to handle a uniaxial deformation MD simulation
     """
     default_params={
-        'subdir':'postsim/deform',
+        'subdir':'postsim/deform-x',
         'input_top': 'systems/final-results/final.top',
         'input_gro': 'postsim/equilibrate/equilibrate.gro',
         'input_grx': 'systems/final-results/final.grx',
@@ -238,19 +254,17 @@ class PostSimDeform(PostSimMD):
             'options': '-quiet -nobackup',
             'mdrun_single_molecule': 'gmx mdrun mdrun'
         },
-        'output_deffnm':'ladder',
-        'traces': ['Temperature','Density','Volume'],
-        'scatter': ('time(ps)',['Density'],'rho_v_ns.png'),
-        'Tlo':300.0,
-        'Thi':600.0,
-        'deltaT':5,
-        'ps_per_run':1000,
-        'ps_per_rise':1000,
-        'warmup_ps':5000,
-        'P':1
+        'output_deffnm':'deform-x',
+        'traces': ['Box-X','Pres-XX'],
+        'scatter': ('Box-X',['Pres-XX'],'tension_v_xlength.png'),
+        'direction':'x',
+        'T':300.0,
+        'P':1.0,
+        'ps':1000,
+        'edot': 0.001 # strain rate in ps^-1
     }
 
-    def build_mdp(self,mdpname):
+    def build_mdp(self,mdpname,**kwargs):
         """build_mdp builds the GROMACS mdp file required for a temperature-ladder MD simulation
 
         :param mdpname: name of mdp file
@@ -258,33 +272,53 @@ class PostSimDeform(PostSimMD):
         """
         params=self.params
         timestep=float(mdp_get(mdpname,'dt'))
-        Tdomain=params['Thi']-params['Tlo']
-        nSteps=int(Tdomain/np.abs(params['deltaT']))+1
-        T0=params['Tlo'] if params['deltaT']>0.0 else params['Thi']
-        T1=params['Thi'] if params['deltaT']>0.0 else params['Tlo']
-        Tladder=np.linspace(T0,T1,nSteps)
-        timepoints=[0.0,params['warmup_ps']]
-        temppoints=[T0,T0]
-        for i in range(nSteps):
-            cT=Tladder[i]
-            timepoints.append(timepoints[-1]+params['ps_per_rise'])
-            temppoints.append(cT)
-            timepoints.append(timepoints[-1]+params['ps_per_run'])
-            temppoints.append(cT)
-        duration=timepoints[-1]
-        nMDsteps=int(duration/timestep)
+        duration=params['ps']
+        nsteps=int(duration/timestep)
+        box=kwargs.get('box',np.array([[0.0,0.0,0.0],[0.0,0.0,0.0],[0.0,0.0,0.0]]))
+        edot=params.get('edot',0.0)
+        direction=params.get('direction','x')
         mod_dict={
-            'ref_t':T0,
+            'ref_t':params['T'],
             'ref_p':params['P'],
-            'gen-temp':T0,
+            'gen-temp':params['T'],
             'gen-vel':'yes',
-            'annealing-npoints':len(timepoints),
-            'annealing-temp':' '.join([str(x) for x in temppoints]),
-            'annealing-time':' '.join([str(x) for x in timepoints]),
-            'annealing':'single',
-            'nsteps':nMDsteps,
-            'tcoupl':'v-rescale','tau_t':0.5
+            'tcoupl':'v-rescale',
+            'nsteps': nsteps,
+            'rlist': 1.2,
+            'rcoulomb': 1.2,
+            'rvdw': 1.2,
+            'tau_t':0.5,
+            'tau_p':1.0,
+            'refcoord_scaling': 'com',
+            'pcoupltype': 'anisotropic'
             }
+        if direction=='x':
+            strain_vel=box[0][0]*edot
+            mod_dict['ref_p']='0.0 1.0 1.0 0 0 0'
+            mod_dict['compressibility']='0.0 4.5e-5 4.5e-5 0 0 0'
+            mod_dict['deform']=f'{strain_vel:.3f} 0 0 0 0 0'
+            params['output_deffnm'] = 'deform-x'
+            params['traces']=['Box-X','Pres-XX']
+            params['scatter']=('Box-X',['Pres-XX'],'tension_v_xlength.png')
+        elif direction=='y':
+            strain_vel=box[1][1]*edot
+            mod_dict['ref_p']='1.0 0.0 1.0 0 0 0'
+            mod_dict['compressibility']='4.5e-5 0.0 4.5e-5 0 0 0'
+            mod_dict['deform']=f'0 {strain_vel:.3f} 0 0 0 0'
+            params['output_deffnm'] = 'deform-y'
+            params['traces']=['Box-Y','Pres-YY']
+            params['scatter']=('Box-Y',['Pres-YY'],'tension_v_ylength.png')
+        elif direction=='z':
+            strain_vel=box[2][2]*edot
+            mod_dict['ref_p']='1.0 1.0 0.0 0 0 0'
+            mod_dict['compressibility']='4.5e-5 4.5e-5 0.0 0 0 0'
+            mod_dict['deform']=f'0 0 {strain_vel:.3f} 0 0 0'
+            params['output_deffnm'] = 'deform-z'
+            params['traces']=['Box-Z','Pres-ZZ']
+            params['scatter']=('Box-Z',['Pres-ZZ'],'tension_v_zlength.png')
+        else:
+            logger.error(f'Bad direction for uniaxial strain {direction}')
+
         mdp_modify(mdpname,mod_dict)
 
 class PostsimConfiguration:
@@ -293,7 +327,6 @@ class PostsimConfiguration:
         
         - { key1: {<paramdict>}}
         - { key2: {<paramdict>}} 
-        
         
         ...
 
@@ -306,10 +339,10 @@ class PostsimConfiguration:
         - 'equilibrate': simple NPT equilibration;
         - 'anneal': simple simulated annealing;
         - 'ladder': temperature ladder;
-        - 'deform': deform along one axis (not yet implemented)
+        - 'deform: constant strain-rate uniaxial deformation;
         
         """
-    default_classes={'equilibrate':PostSimMD,'anneal':PostSimAnneal,'ladder':PostSimLadder,'deform':None}
+    default_classes={'equilibrate':PostSimMD,'anneal':PostSimAnneal,'ladder':PostSimLadder,'deform':PostSimDeform}
     def __init__(self):
         self.cfgFile=''
         self.baselist=[]
