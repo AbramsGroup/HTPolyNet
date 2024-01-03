3432f38272d57019990fa72ab76db9858405cb71
Cameron Abrams <cfa22@drexel.edu>
2024-01-03 15:36:17 -0500

    v1_0_8_rc7

--- HTPolyNet/banner.py
+++ HTPolyNet/banner.py
@@ -21,6 +21,13 @@ banner_message="""
 
     Supported in part by Grants W911NF-17-2-0227 
     and W911NF-12-R-0011 from the US Army Research Lab
+
+    Please cite the HTPolyNet paper:
+    
+    Ming Huang and Cameron F. Abrams, HTPolyNet: A general 
+    system generator for all-atom molecular simulations of 
+    amorphous crosslinked polymers, SoftwareX, vol. 21, 
+    pp. 101303, 2023 (doi:10.1016/j.softx.2022.101303)
     """.format(HTPOLYNET_VERSION)
 def banner(logf):
     my_logger(banner_message,logf,fill=' ',just='<')
